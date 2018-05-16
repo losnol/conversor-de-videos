054b23afb1999e4ce6122e49f2aa73138fa54b64
losnol <losnolzin@gmail.com>
2018-05-16 01:35:49 -0300

    Adicionando página para verificar se o usuário quer ver o vídeo ou não.

---
+++ src/main/java/br/com/app/output/ConfirmacaoVideo.java
@@ -0,0 +1,21 @@
+package br.com.app.output;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ConfirmacaoVideo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println((String) req.getParameter("urlvideo"));
+		resp.sendRedirect((String) req.getParameter("urlvideo"));
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
---
+++ src/main/webapp/confirmacaovideo.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Blau</title>
+</head>
+<body>
+	<form action="confirmacaovideo" method="GET">
+		<input type="hidden" name="urlvideo" value= ${urlvideo} >
+		<input type="submit" value="Ver o Vídeo" />
+	</form>
+	<form method="get" action="index">
+		<input type="submit" value="Retornar a página principal" />
+	</form>
+</body>
+</html>
